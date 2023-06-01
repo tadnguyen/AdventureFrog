808039d9d099c040eaba0679b07e090b191fcf9d
Tadnguyen <jokermr199x@gmail.com>
2023-06-01 17:55:03 +0700

    push project to git

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int bananas = 0;
+    [SerializeField] private Text bananasText;
+    [SerializeField] private AudioSource collectSoundEffect;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.gameObject.CompareTag("Banana"))
+        {
+            collectSoundEffect.Play();
+            Destroy(collision.gameObject);
+            bananas++;
+            bananasText.text = "Bananas: " + bananas;
+        }  
+    } 
+}
